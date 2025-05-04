bc6f7cb4c39b4384e0507f14b972cee9c0d2c040
Martingilg <teemailcimed@pgit config --global user.name Martingilglda.com>
2025-05-04 11:27:48 +0200

    frissitett zenek.sql

--- adat/zenek.sql
+++ adat/zenek.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d31261a3b1107437ad334de40f3ff263841d7f42abdb4841ee2c00c4a54880e
-size 197217628
+oid sha256:65915bd8537a907acd3754d8f86025f3da1df1686ea5bcf61b486d7b7b1ae692
+size 197217711
